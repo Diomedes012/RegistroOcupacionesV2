2dcfbc1a982402d0cc5d0fdc42062741ec6264c5
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-05-09 23:25:55 -0400

    Crea UpsertOcupacionUseCase

---
+++ app/src/main/java/ucne/edu/registroocupacionesv2/domain/tareas/usecase/UpsertOcupacionUseCase.kt
@@ -0,0 +1,29 @@
+package ucne.edu.registroocupacionesv2.domain.tareas.usecase
+
+import ucne.edu.registroocupacionesv2.domain.tareas.model.Ocupacion
+import ucne.edu.registroocupacionesv2.domain.tareas.repository.OcupacionRepository
+import javax.inject.Inject
+
+class UpsertOcupacionUseCase @Inject constructor(private val repository: OcupacionRepository) {
+    suspend operator fun invoke(ocupacion: Ocupacion): Result<Int>
+    {
+        if(ocupacion.descripcion.isBlank())
+        {
+            return Result.failure(IllegalArgumentException("Descripcion es Obligatoria"))
+        }
+
+        if(ocupacion.sueldo <= 0.0)
+        {
+            return Result.failure(IllegalArgumentException("El sueldo debe ser mayor a cero"))
+        }
+
+        val existe = repository.existsByDescripcion(ocupacion.descripcion.trim())
+
+        if(existe && ocupacion.ocupacionId == 0)
+        {
+            return Result.failure(IllegalArgumentException("Ya existe ocupacion con esta descripcion"))
+        }
+
+        return runCatching { repository.upsert(ocupacion) }
+    }
+}
