e4fa2dc2207664baafa28940a4185db6294dbb86
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-05-18 19:40:44 -0400

    Refactor a OcupacionDb.kt

--- app/src/main/java/ucne/edu/registroocupacionesv2/data/database/OcupacionDb.kt
+++ app/src/main/java/ucne/edu/registroocupacionesv2/data/database/OcupacionDb.kt
@@ -2,13 +2,18 @@ package ucne.edu.registroocupacionesv2.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import ucne.edu.registroocupacionesv2.data.empleados.local.EmpleadoDao
+import ucne.edu.registroocupacionesv2.data.empleados.local.EmpleadoEntity
 import ucne.edu.registroocupacionesv2.data.ocupaciones.local.OcupacionDao
 import ucne.edu.registroocupacionesv2.data.ocupaciones.local.OcupacionEntity
 
 @Database(
-    entities = [OcupacionEntity::class],
-    version = 1
+    entities = [OcupacionEntity::class, EmpleadoEntity::class],
+    version = 2
 )
+
 abstract class OcupacionDb: RoomDatabase() {
     abstract fun ocupacionDao(): OcupacionDao
+
+    abstract fun empleadoDao(): EmpleadoDao
 }
