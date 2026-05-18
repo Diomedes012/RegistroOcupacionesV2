61f141c800f02010c29fc4fd499e3dd734c0c651
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-05-18 19:40:07 -0400

    Refactor a RepositoryModule.kt para funcionar con Empleado

--- app/src/main/java/ucne/edu/registroocupacionesv2/di/RepositoryModule.kt
+++ app/src/main/java/ucne/edu/registroocupacionesv2/di/RepositoryModule.kt
@@ -4,7 +4,9 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import ucne.edu.registroocupacionesv2.data.empleados.repository.EmpleadoRepositoryImpl
 import ucne.edu.registroocupacionesv2.data.ocupaciones.repository.OcupacionRepositoryImpl
+import ucne.edu.registroocupacionesv2.domain.empleados.repository.EmpleadoRepository
 import ucne.edu.registroocupacionesv2.domain.ocupaciones.repository.OcupacionRepository
 import javax.inject.Singleton
 
@@ -14,4 +16,8 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindOcupacionRepository(impl: OcupacionRepositoryImpl): OcupacionRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindEmpleadoRepository(impl: EmpleadoRepositoryImpl): EmpleadoRepository
 }
