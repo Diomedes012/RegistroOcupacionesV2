7f66c1f4829264bd0a7c8be90c72cca1556d4a00
Gregory Junior Rodríguez Serrano <gregory_rodriguez@ucne.edu.do>
2026-05-17 23:36:43 -0400

    Crea EmpleadoFormUiState

---
+++ app/src/main/java/ucne/edu/registroocupacionesv2/presentation/empleados/form/EmpleadoFormUiState.kt
@@ -0,0 +1,20 @@
+package ucne.edu.registroocupacionesv2.presentation.empleados.form
+
+data class EmpleadoFormUiState(
+    val empleadoId: Int? = null,
+    val nombres: String = "",
+    val sexo: String = "",
+    val fechaIngreso: String = "",
+    val sueldo: String = "",
+
+    val nombresError: String? = null,
+    val sexoError: String? = null,
+    val fechaIngresoError: String? = null,
+    val sueldoError: String? = null,
+
+    val isSaving: Boolean = false,
+    val isDeleting: Boolean = false,
+    val isNew: Boolean = true,
+    val saved: Boolean = false,
+    val deleted: Boolean = false
+)
